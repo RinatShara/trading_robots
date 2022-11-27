2dc6e543461b22515ffc0e004f7245b54b082b05
RinatShara <rsharapov54a@gmail.com>
2022-11-27 15:18:14 +0500

    test merging 2

--- tokens.py
+++ tokens.py
@@ -2,3 +2,4 @@ ro_token = 't.KDkYqpvaGFBrQSWf2WuUdYyGwYjP2tfsQWqifW0YJuWHKXilu7zs_XPaPigYmLuL48
 sb_token = 't.ucBvgrQDAM8ebcyG1SgrtPlAOuujKtQoA8sAC-9AIwQJp_9iGqjnpAM5RC7Vny46FLSZ-HrIO95-Ch6UYywjXg'
 '''testing merging'''
 '''hello'''
+'''add something'''
