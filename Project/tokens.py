a8f2964b0f31d6b73bb1570e8f8c474321651405
RinatShara <rsharapov54a@gmail.com>
2023-09-04 08:33:52 -0700

    remove file with all tokens

--- Project/tokens.py
+++
@@ -1,2 +0,0 @@
-ro_token = 't.KDkYqpvaGFBrQSWf2WuUdYyGwYjP2tfsQWqifW0YJuWHKXilu7zs_XPaPigYmLuL48N4fxvhS2NBoZ-LGsctrw'
-sb_token = 't.ucBvgrQDAM8ebcyG1SgrtPlAOuujKtQoA8sAC-9AIwQJp_9iGqjnpAM5RC7Vny46FLSZ-HrIO95-Ch6UYywjXg'
